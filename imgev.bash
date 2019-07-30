18d96f55108b0281c3a279987e4292b3bf8860c1
Ruben Soria <ruben.soria@alumnos.unican.es>
2019-07-30 10:49:25 +0200

    Initial version of SIXTE simulations

---
+++ imgev.bash
@@ -0,0 +1,8 @@
+imgev \
+EvtFile=sim_evt_wfi_hi_casa.fits \
+Image=casa_wfi_hi_img.fits \
+CoordinateSystem=0 Projection=TAN \
+NAXIS1=512 NAXIS2=512 CUNIT1=deg CUNIT2=deg \
+CRVAL1=350.91757 CRVAL2=58.7934 CRPIX1=256.5 CRPIX2=256.5 \
+CDELT1=-6.207043e-04 CDELT2=6.207043e-04 \
+history=true clobber=yes
